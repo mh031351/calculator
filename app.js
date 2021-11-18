28683190666107196493e72b7d987f381c24debe
mh031351 <mh031351>
2021-11-18 10:36:43 -0800

    Work Complete

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+let outputscreen = document.getElementById("output-screen");
+        
+function display(num){
+    outputscreen.value += num;
+}
+function calculate(){
+    try
+    {
+        outputscreen.value = eval(outputscreen.value)
+
+    }
+    catch(err)
+    {
+        alert("WRONG QUETION")
+
+    }
+
+
+}
+function Clear(){
+    outputscreen.value = "";
+
+}
+
+function del (){
+    outputscreen.value = outputscreen.value.slice(0,-2)
+}
